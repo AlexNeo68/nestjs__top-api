8a1807cffdbfff159a5dbd31f0cca9a9aba2efa6
Александр Неудахин <aleksandrneudahin@Aleksandrs-MacBook-Air.local>
2023-07-13 08:23:37 +0300

    controllers

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { FindTopPageDto } from 'src/top-page/dto/find-top-page.dto';
 import { TopPageModel } from 'src/top-page/top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
-    @Post('create')
+  @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
   @Get(':id')
@@ -19,6 +28,4 @@ export class TopPageController {
   @HttpCode(200)
   @Post('find')
   async find(@Body() dto: FindTopPageDto) {}
-}
-
 }
