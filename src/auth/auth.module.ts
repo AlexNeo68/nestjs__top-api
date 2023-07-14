cfe20c0f7afe8a6ffa41356b6fa09fc4ebd09b60
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2023-07-14 08:14:09 +0300

    define models

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModel } from 'src/auth/auth.model';
 
 @Module({
   controllers: [AuthController],
   providers: [ConfigService],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModel,
+        schemaOptions: {
+          collection: 'User',
+        },
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,7 +1,17 @@
-export class ReviewModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+  @prop()
   title: string;
+
+  @prop()
   description: string;
+
+  @prop()
   name: string;
+
+  @prop()
   rating: number;
 }
